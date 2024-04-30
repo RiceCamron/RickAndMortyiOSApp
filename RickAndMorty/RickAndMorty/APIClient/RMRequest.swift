b598c98723e43b04bf8bc16aa7c5367cb52d904b
RiceCamron <60075263+RiceCamron@users.noreply.github.com>
2024-04-30 17:28:52 +0500

    commit2

---
+++ RickAndMorty/RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Artur Avdeev on 30.04.2024.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
